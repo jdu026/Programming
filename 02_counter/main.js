442b2a230d5f868c274d3d77a1f407a14097fe13
jdu026 <s2434@e-mirim.hs.kr>
2026-03-24 09:52:55 +0900

    counter HTML onclick

---
+++ 02_counter/main.js
@@ -0,0 +1,51 @@
+let count = 0;
+
+//html -> js
+// function count(){
+//     const result = document.getElementById("result");
+
+//     let number = 
+
+// }
+
+// const resultH1 = document.querySelectorAll("h1")[0];
+
+// const resultH1 = document.getElementsByTagName("h1")[0];
+// const plusbutton = document.getElementsByTagName("button")[0];
+// const minusbutton = document.get
+
+// plusbutton.addEventListener("click", function (){
+//     //숫자 증가
+//         count++; //count += 1; // count = count +1;
+    
+//     //숫자 표시
+//     resultH1.innerHTML= count;
+// });
+
+const resultH1 = document.getElementById("result");
+const plusButton = document.getElementsByClassName("plusBtn")[0];
+
+// plusbutton.addEventListener("click", 처리함수);
+
+// function 처리함수(){
+//     //숫자 증가
+//         count++; //count += 1; // count = count +1;
+    
+//     //숫자 표시
+//     resultH1.innerHTML= count;
+// }
+// plusbutton.onclick = () => {
+
+//     //숫자 증가
+//         count++; //count += 1; // count = count +1;
+    
+//     //숫자 표시
+//     resultH1.innerHTML= count;
+// };
+function plus(){
+    //숫자 증가
+        count++; //count += 1; // count = count +1;
+    
+    //숫자 표시
+    resultH1.innerHTML= count;
+}
