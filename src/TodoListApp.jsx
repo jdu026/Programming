36a62b5e35acb6b6215b5e8e04f293040c0b2635
jdu026 <s2434@e-mirim.hs.kr>
2026-04-14 10:11:07 +0900

    TodoListApp, addTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -9,12 +9,34 @@ import TodoHeader from "./components/TodoHeader.jsx";
 import TodoAdder from "./components/TodoAdder.jsx";
 import TodoItem from "./components/TodoItem.jsx";
 import TodoList from "./components/TodoList.jsx";
+import { useState } from 'react';
+
+class Todo {
+  constructor(id, text, isCompleted) {
+    this.id = id;
+    this.text = text;
+    this.isCompleted = isCompleted;
+  }
+}
 
 function TodoListApp() {
+  const [todos, setTodos] = useState([]);
+  function addTodo(text){
+    //이전 todos에 newTodo 만들어서 추가하자 -> 그것을 setTodos() 하자
+    setTodos((todos) => [
+      ...todos, //todos에 있는 item을 다 꺼내서 새로운 리스트에 하나씨 넣자
+      new Todo(
+        Date.now(), //id: 고유 ID 시간을 이용 == new Date().getTime()
+        text, //text:할 일 내용
+        false //isCompleted: 할 일의 완료 여부 초기값은 false
+      )
+    ]);  
+  }
+
   return (
     <div className="todo">
       <TodoHeader />
-      <TodoAdder />
+      <TodoAdder addTodo={addTodo}/>
       <TodoList />
     </div>
   )
--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import Button from './Button.jsx';
 
-export default function TodoAdder() {
+
+export default function TodoAdder({addTodo}) {
+    const [inputTodo, setInputTodo] = useState('');
+    const handleSumbit = (event) => {
+        event.preventDefault(); //기본 동작 막자
+        if(!inputTodo.trim()) return; //빈칸이면 바로 return
+        addTodo(inputTodo.trim()); //input에 있는 text value 할 일 하기 뒤
+        setInputTodo('');
+    }
+
+
     return (
-        <form className="todo__form">
-            <input type="text" className="todo__input" placeholder="할 일을 입력하세요" />
+        <form className="todo__form" onSubmit={handleSumbit}>
+            <input type="text" className="todo__input" placeholder="할 일을 입력하세요" value = {inputTodo} onChange = {(event) => setInputTodo(event.target.value)}/>
             <Button type="submit" className="todo__button todo__button--add">Add</Button>
         </form>
     );
