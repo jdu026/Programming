a27b1ea04fbb3f7bc8e14630707d9415a21041bf
jdu026 <s2434@e-mirim.hs.kr>
2026-04-21 10:10:25 +0900

    TodoListApp toggleTodo()

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,16 @@
 import CheckBox from "./CheckBox.jsx";
 import Button from './Button.jsx';
+import Checkbox from "./CheckBox.jsx";
 
-export default function TodoItem({todo}) {
+export default function TodoItem({ todo, toggleTodo }) {
     return (
         // todo.isCompleted가 true면 " todo__item--complete", false ""
-        <li className={`todo__item${todo.isCompleted? " todo_item--complete" : ""}`}>
-            <CheckBox type="checkBox" className="todo__check" id={todo.id}>{todo.text}</CheckBox>
+        <li className={`todo__item${todo.isCompleted ? " todo__item--complete" : ""}`}>
+            <CheckBox 
+                id={todo.id}
+                checked={todo.isCompleted}
+                onChange={() => toggleTodo(todo.id)}
+                >{todo.text}</CheckBox>
             <Button className="todo__button todo__button--edit">🖋️</Button>
             <Button className="todo__button todo__button--delete">🚮</Button>
         </li>
